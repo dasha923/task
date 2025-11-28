60a4e8784a3daa4803a2825f604100221b696bc6
dasha923 <dashayuditskaya@yandex.ru>
2025-11-28 10:13:51 +0700

    Delete local/templates/tex directory

--- local/templates/tex/components/bitrix/form.result.new/build/description.php
+++
@@ -1,8 +0,0 @@
-<?
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-
-$arTemplateDescription = array(
-    'NAME' => ' - Контактная форма',
-    'DESCRIPTION' => 'build Кастомный шаблон для формы обратной связи'
-);
-?>
